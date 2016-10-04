4947b23e2add820bae14635db31938508028563b
Antoine Lorence <alorence@flux.to>
2016-10-04 11:58:38 +0200

    Fixed run_tests return code

--- run_tests.py
+++ run_tests.py
@@ -8,11 +8,18 @@ import pytest
 
 if __name__ == '__main__':
 
+    return_codes = []
+
     print('Running py.test')
     ret = pytest.main()
     print('py.test failed' if ret else 'py.test passed')
+    return_codes.append(ret)
 
     print('Running flake8')
     ret = subprocess.call(['flake8'])
     print('flake8 failed' if ret else 'flake8 passed')
+    return_codes.append(ret)
+
     print('')
+
+    exit(0 if all([x == 0 for x in return_codes]) else 1)
