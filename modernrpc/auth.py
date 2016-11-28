cbb270b8dfa957eb65cf8683e8adbc0767300617
Antoine Lorence <alorence@flux.to>
2016-11-28 21:27:07 +0100

    Remove dead code
    @permission_required cannot be used without arguments

--- modernrpc/auth.py
+++ modernrpc/auth.py
@@ -74,7 +74,7 @@ def superuser_required(func=None):
     return decorated(func)
 
 
-def permissions_required(func=None, permissions=None):
+def permissions_required(func, permissions):
     def decorated(function):
         if not permissions:
             raise ImproperlyConfigured('When using @permissions_required() decorator, you must provide the '
@@ -84,12 +84,6 @@ def permissions_required(func=None, permissions=None):
             return user_pass_test(function, check_user_has_perms, [permissions])
 
         return user_pass_test(function, check_user_has_perm, [permissions])
-
-    # If @rpc_method is used without any argument nor parenthesis
-    if func is None:
-        def decorator(f):
-            return decorated(f)
-        return decorator
 
     # If @rpc_method() is used with parenthesis (with or without arguments)
     return decorated(func)
