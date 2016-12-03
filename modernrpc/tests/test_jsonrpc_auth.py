0fe922bb4b98d4e38b796ffcf7259170880d0fb9
Antoine Lorence <alorence@flux.to>
2016-12-03 09:46:30 +0100

    Split auth related tests into multiple functions

--- modernrpc/tests/test_jsonrpc_auth.py
+++ modernrpc/tests/test_jsonrpc_auth.py
@@ -9,21 +9,15 @@ def test_jsrpc_user_is_logged(live_server, john_doe):
     client = ServerProxy(live_server.url + '/all-rpc/', login=john_doe.username, password='123456')
     assert client.logged_user_required(5) == 25
 
+
+def test_jsrpc_user_is_not_logged(live_server):
+
     with pytest.raises(ProtocolError):
         client = ServerProxy(live_server.url + '/all-rpc/')
         client.logged_user_required(4)
 
 
-def test_jsrpc_user_is_admin(live_server, john_doe, superuser):
-
-    client = ServerProxy(live_server.url + '/all-rpc/', login=superuser.username, password='123456')
-    # Admin is OK
-    assert client.logged_superuser_required(5) == 15
-
-    with pytest.raises(ProtocolError):
-        # JohnDoe don't have sufficient permissions
-        client = ServerProxy(live_server.url + '/all-rpc/', login=john_doe.username, password='123456')
-        client.logged_superuser_required(5)
+def test_jsrpc_anon_is_superuser(live_server):
 
     with pytest.raises(ProtocolError):
         # Anonymous user don't have sufficient permissions
@@ -31,11 +25,30 @@ def test_jsrpc_user_is_admin(live_server, john_doe, superuser):
         client.logged_superuser_required(4)
 
 
-def test_jsrpc_user_has_single_permission(live_server, john_doe, superuser, auth_permissions):
+def test_jsrpc_user_is_superuser(live_server, john_doe):
 
-    # Passing superuser credential always works
-    client = ServerProxy(live_server.url + '/all-rpc/', superuser.username, '123456')
-    assert client.delete_user_perm_required(5) == 5
+    with pytest.raises(ProtocolError):
+        # JohnDoe don't have sufficient permissions
+        client = ServerProxy(live_server.url + '/all-rpc/', login=john_doe.username, password='123456')
+        client.logged_superuser_required(5)
+
+
+def test_jsrpc_is_superuser_ok(live_server, superuser):
+
+    client = ServerProxy(live_server.url + '/all-rpc/', login=superuser.username, password='123456')
+    # Superuser is OK
+    assert client.logged_superuser_required(5) == 15
+
+
+def test_jsrpc_anon_has_single_permission(live_server, auth_permissions):
+
+    with pytest.raises(ProtocolError):
+        # Anonymous user don't have sufficient permissions
+        client = ServerProxy(live_server.url + '/all-rpc/')
+        client.logged_superuser_required(4)
+
+
+def test_jsrpc_user_has_single_permission(live_server, john_doe, auth_permissions):
 
     # John Doe doesn't have permission to execute the method...
     with pytest.raises(ProtocolError):
@@ -52,17 +65,23 @@ def test_jsrpc_user_has_single_permission(live_server, john_doe, superuser, auth
 
     john_doe.user_permissions.clear()
 
+
+def test_jsrpc_superuser_has_single_permission(live_server, superuser):
+
+    # Passing superuser credential always works
+    client = ServerProxy(live_server.url + '/all-rpc/', superuser.username, '123456')
+    assert client.delete_user_perm_required(5) == 5
+
+
+def test_jsrpc_anon_has_multiple_permissions(live_server):
+
     with pytest.raises(ProtocolError):
         # Anonymous user don't have sufficient permissions
         client = ServerProxy(live_server.url + '/all-rpc/')
         client.logged_superuser_required(4)
 
 
-def test_jsrpc_user_has_multiple_permissions(live_server, john_doe, superuser, auth_permissions):
-
-    # Passing superuser credential always works
-    client = ServerProxy(live_server.url + '/all-rpc/', superuser.username, '123456')
-    assert client.delete_user_perms_required(5) == 5
+def test_jsrpc_user_has_multiple_permissions(live_server, john_doe, auth_permissions):
 
     # John Doe doesn't have permission to execute the method...
     with pytest.raises(ProtocolError):
@@ -84,7 +103,9 @@ def test_jsrpc_user_has_multiple_permissions(live_server, john_doe, superuser, a
 
     john_doe.user_permissions.clear()
 
-    with pytest.raises(ProtocolError):
-        # Anonymous user don't have sufficient permissions
-        client = ServerProxy(live_server.url + '/all-rpc/')
-        client.logged_superuser_required(4)
+
+def test_jsrpc_superuser_has_multiple_permissions(live_server, superuser):
+
+    # Passing superuser credential always works
+    client = ServerProxy(live_server.url + '/all-rpc/', superuser.username, '123456')
+    assert client.delete_user_perms_required(5) == 5
--- modernrpc/tests/test_xmlrpc_auth.py
+++ modernrpc/tests/test_xmlrpc_auth.py
@@ -11,25 +11,17 @@ def test_xrpc_user_is_logged(live_server, john_doe):
     client = xmlrpc_client.ServerProxy(auth_url)
     assert client.logged_user_required(5) == 25
 
+
+def test_xrpc_user_is_not_logged(live_server):
+
     with pytest.raises(xmlrpc_client.ProtocolError):
-        client = xmlrpc_client.ServerProxy(orig_url)
+        client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
         client.logged_user_required(4)
 
 
-def test_xrpc_user_is_admin(live_server, john_doe, superuser):
+def test_xrpc_anon_is_superuser(live_server):
 
     orig_url = live_server.url + '/all-rpc/'
-    johndoe_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(john_doe.username))
-    admin_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
-
-    client = xmlrpc_client.ServerProxy(admin_auth_url)
-    # Admin is OK
-    assert client.logged_superuser_required(5) == 15
-
-    with pytest.raises(xmlrpc_client.ProtocolError):
-        # JohnDoe don't have sufficient permissions
-        client = xmlrpc_client.ServerProxy(johndoe_auth_url)
-        client.logged_superuser_required(5)
 
     with pytest.raises(xmlrpc_client.ProtocolError):
         # Anonymous user don't have sufficient permissions
@@ -37,15 +29,41 @@ def test_xrpc_user_is_admin(live_server, john_doe, superuser):
         client.logged_superuser_required(4)
 
 
-def test_xrpc_user_has_single_permission(live_server, john_doe, superuser, auth_permissions):
+def test_xrpc_user_is_superuser(live_server, john_doe):
 
     orig_url = live_server.url + '/all-rpc/'
     johndoe_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(john_doe.username))
-    admin_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
 
-    # Passing superuser credential always works
-    client = xmlrpc_client.ServerProxy(admin_auth_url)
-    assert client.delete_user_perm_required(5) == 5
+    with pytest.raises(xmlrpc_client.ProtocolError):
+        # JohnDoe don't have sufficient permissions
+        client = xmlrpc_client.ServerProxy(johndoe_auth_url)
+        client.logged_superuser_required(5)
+
+
+def test_xrpc_is_superuser_ok(live_server, superuser):
+
+    orig_url = live_server.url + '/all-rpc/'
+    superuser_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
+
+    client = xmlrpc_client.ServerProxy(superuser_auth_url)
+    # Superuser is OK
+    assert client.logged_superuser_required(5) == 15
+
+
+def test_xrpc_anon_has_single_permission(live_server):
+
+    orig_url = live_server.url + '/all-rpc/'
+
+    with pytest.raises(xmlrpc_client.ProtocolError):
+        # Anonymous user don't have sufficient permissions
+        client = xmlrpc_client.ServerProxy(orig_url)
+        client.logged_superuser_required(4)
+
+
+def test_xrpc_user_has_single_permission(live_server, john_doe, auth_permissions):
+
+    orig_url = live_server.url + '/all-rpc/'
+    johndoe_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(john_doe.username))
 
     # John Doe doesn't have permission to execute the method...
     with pytest.raises(xmlrpc_client.ProtocolError):
@@ -62,21 +80,31 @@ def test_xrpc_user_has_single_permission(live_server, john_doe, superuser, auth_
 
     john_doe.user_permissions.clear()
 
+
+def test_xrpc_superuser_has_single_permission(live_server, superuser, auth_permissions):
+
+    orig_url = live_server.url + '/all-rpc/'
+    superuser_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
+
+    # Passing superuser credential always works
+    client = xmlrpc_client.ServerProxy(superuser_auth_url)
+    assert client.delete_user_perm_required(5) == 5
+
+
+def test_xrpc_anon_has_multiple_permissions(live_server):
+
+    orig_url = live_server.url + '/all-rpc/'
+
     with pytest.raises(xmlrpc_client.ProtocolError):
         # Anonymous user don't have sufficient permissions
         client = xmlrpc_client.ServerProxy(orig_url)
         client.logged_superuser_required(4)
 
 
-def test_xrpc_user_has_multiple_permissions(live_server, john_doe, superuser, auth_permissions):
+def test_xrpc_user_has_multiple_permissions(live_server, john_doe, auth_permissions):
 
     orig_url = live_server.url + '/all-rpc/'
     johndoe_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(john_doe.username))
-    admin_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
-
-    # Passing superuser credential always works
-    client = xmlrpc_client.ServerProxy(admin_auth_url)
-    assert client.delete_user_perms_required(5) == 5
 
     # John Doe doesn't have permission to execute the method...
     with pytest.raises(xmlrpc_client.ProtocolError):
@@ -98,7 +126,12 @@ def test_xrpc_user_has_multiple_permissions(live_server, john_doe, superuser, au
 
     john_doe.user_permissions.clear()
 
-    with pytest.raises(xmlrpc_client.ProtocolError):
-        # Anonymous user don't have sufficient permissions
-        client = xmlrpc_client.ServerProxy(orig_url)
-        client.logged_superuser_required(4)
+
+def test_xrpc_superuser_has_multiple_permissions(live_server, superuser):
+
+    orig_url = live_server.url + '/all-rpc/'
+    superuser_auth_url = orig_url.replace('http://', 'http://{}:123456@'.format(superuser.username))
+
+    # Passing superuser credential always works
+    client = xmlrpc_client.ServerProxy(superuser_auth_url)
+    assert client.delete_user_perms_required(5) == 5
