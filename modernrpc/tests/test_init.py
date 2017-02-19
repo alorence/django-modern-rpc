5267662a7d0492f21c97d3697a44fdf08fc57f7e
Antoine Lorence <alorence@flux.to>
2017-02-19 09:42:05 +0100

    Adds apps init tests
    Are we finally at 100% coverage ?

---
+++ modernrpc/tests/test_init.py
@@ -0,0 +1,34 @@
+import pytest
+from django.apps.registry import apps
+
+from modernrpc.core import get_all_method_names
+
+
+def test_init_missing_method_modules_setting(settings):
+
+    settings.MODERNRPC_METHODS_MODULES = []
+    app = apps.get_app_config("modernrpc")
+
+    with pytest.warns(Warning):
+        app.ready()
+
+
+def test_init_registry_cleaning(settings):
+
+    app = apps.get_app_config("modernrpc")
+
+    # Normal initialization: RPC methods from custom modules are registered
+    app.ready()
+
+    # Retrieve all methods defined in custom modules (not system.* methods)
+    custom_rpc_methods = [m for m in get_all_method_names() if not m.startswith('system.')]
+    # Ensure a normal init registered some custom methods
+    assert len(custom_rpc_methods) > 0
+
+    # Re-init library, with only invalid module as "methods module"
+    settings.MODERNRPC_METHODS_MODULES = ['nope']
+    app.ready()
+
+    # Now, we should have removed all custom methods from the registry
+    custom_rpc_methods = [m for m in get_all_method_names() if not m.startswith('system.')]
+    assert len(custom_rpc_methods) == 0
