6a8e918ee2028fd2ed9bd4b80d237e341e3d9244
Antoine Lorence <antoine.lorence@gmail.com>
2017-06-29 08:50:34 +0200

    Add unit tests

---
+++ modernrpc/tests/test_utils.py
@@ -0,0 +1,14 @@
+from modernrpc.utils import ensure_sequence
+
+
+def test_ensure_sequence_1():
+
+    assert ensure_sequence([1, 2, 3]) == [1, 2, 3]
+    assert ensure_sequence(('a', 'b', 'c')) == ('a', 'b', 'c')
+
+
+def test_ensure_sequence_2():
+
+    assert ensure_sequence('str') == ['str']
+    assert ensure_sequence(666) == [666]
+    assert ensure_sequence({'x': 42}) == [{'x': 42}]
