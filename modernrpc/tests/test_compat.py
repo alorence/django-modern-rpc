0c9c0a15a6c0928bc2322dae1e2a00329191c70d
Antoine Lorence <antoine.lorence@gmail.com>
2017-09-25 10:23:38 +0200

    Fix py2/py3 specific tests reason (typo)

--- modernrpc/tests/test_compat.py
+++ modernrpc/tests/test_compat.py
@@ -8,51 +8,51 @@ from pytest import raises
 from modernrpc.compat import standardize_strings
 
 
-@pytest.mark.skipif(six.PY2, reason='This test if for Python 3 only')
+@pytest.mark.skipif(six.PY2, reason='Python 3 specific test')
 def test_standardize_str_error_with_py3():
     with raises(AssertionError):
         standardize_strings('123')
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_1():
     assert standardize_strings('abc', unicode) == u'abc'
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_2():
     assert standardize_strings(u'abc', str) == 'abc'
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_3():
     in_list = [145, 964, 84, ['ghjfgh', 64], [[84, 9.254, b'trdf', 645], '456', u'784', 'sdfg']]
     expected_out = [145, 964, 84, [u'ghjfgh', 64], [[84, 9.254, b'trdf', 645], u'456', u'784', u'sdfg']]
     assert standardize_strings(in_list, unicode) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_4():
     in_list = [145, 964, 84, [u'ghjfgh', 64], [[84, 9.254, b'trdf', 645], u'456', u'784', 'sdfg']]
     expected_out = [145, 964, 84, ['ghjfgh', 64], [[84, 9.254, b'trdf', 645], '456', '784', 'sdfg']]
     assert standardize_strings(in_list, str) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_5():
     in_list = (145, 964, 84, ['ghjfgh', 64], [(84, 9.254, b'trdf', 645), '456', u'784', 'sdfg'])
     expected_out = (145, 964, 84, [u'ghjfgh', 64], [(84, 9.254, b'trdf', 645), u'456', u'784', u'sdfg'])
     assert standardize_strings(in_list, unicode) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_6():
     in_list = (145, 964, 84, (u'ghjfgh', 64), ([84, 9.254, b'trdf', 645], u'456', u'784', 'sdfg'))
     expected_out = (145, 964, 84, ('ghjfgh', 64), ([84, 9.254, b'trdf', 645], '456', '784', 'sdfg'))
     assert standardize_strings(in_list, str) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_7():
     in_dict = {
         'a': 456,
@@ -73,7 +73,7 @@ def test_standardize_str_7():
     assert standardize_strings(in_dict, unicode) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_8():
     in_dict = {
         'a': 456,
@@ -94,18 +94,18 @@ def test_standardize_str_8():
     assert standardize_strings(in_dict, str) == expected_out
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_9():
     assert standardize_strings(54, None) == 54
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_standardize_str_10():
     with raises(TypeError):
         assert standardize_strings("64", int)
 
 
-@pytest.mark.skipif(six.PY3, reason='This test if for Python 2 only')
+@pytest.mark.skipif(six.PY3, reason='Python 2 specific test')
 def test_method_level_str_std(live_server):
 
     jclient = HTTPClient(live_server.url + '/all-rpc/')
