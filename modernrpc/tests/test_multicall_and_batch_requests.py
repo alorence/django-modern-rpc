2800ed408b6403239f7303b23c95ccab92b10c07
Antoine Lorence <antoine.lorence@gmail.com>
2017-10-02 22:07:13 +0200

    Added tests for authentication in system.multicall()

--- modernrpc/tests/test_multicall_and_batch_requests.py
+++ modernrpc/tests/test_multicall_and_batch_requests.py
@@ -6,6 +6,7 @@ from django.utils.six.moves import xmlrpc_client
 from jsonrpcclient.http_client import HTTPClient
 
 from modernrpc.exceptions import RPC_METHOD_NOT_FOUND, RPC_INTERNAL_ERROR
+from test_authentication_system import get_url_with_auth
 
 
 def test_xmlrpc_multicall_standard(live_server):
@@ -60,6 +61,36 @@ def test_xmlrpc_multicall_with_errors_2(live_server):
         print(result[1])
     assert excinfo.value.faultCode == RPC_INTERNAL_ERROR
     assert result[2] == 16
+
+
+def test_multicall_with_auth(live_server):
+    client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
+
+    multicall = xmlrpc_client.MultiCall(client)
+    multicall.add(7, 3)
+    multicall.logged_superuser_required(5)
+    result = multicall()
+
+    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert result[0] == 10
+    with pytest.raises(xmlrpc_client.Fault) as excinfo:
+        print(result[1])
+    assert excinfo.value.faultCode == RPC_INTERNAL_ERROR
+    assert 'Authentication failed' in excinfo.value.faultString
+
+
+def test_multicall_with_auth_2(live_server, superuser, common_pwd):
+    server_url = get_url_with_auth(live_server.url + '/all-rpc/', superuser.username, common_pwd)
+    client = xmlrpc_client.ServerProxy(server_url)
+
+    multicall = xmlrpc_client.MultiCall(client)
+    multicall.add(7, 3)
+    multicall.logged_superuser_required(5)
+    result = multicall()
+
+    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert result[0] == 10
+    assert result[1] == 5
 
 
 def test_jsonrpc_batch_standard(live_server):
