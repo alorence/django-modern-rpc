25e956efe742b7d2a3e9a9ab4e804c699823480c
Antoine Lorence <alorence@flux.to>
2017-10-19 16:52:42 +0200

    Oops, getattr does not accept named 'default' arg

--- modernrpc/handlers/jsonhandler.py
+++ modernrpc/handlers/jsonhandler.py
@@ -168,7 +168,7 @@ class JSONRPCHandler(RPCHandler):
             }
         }
 
-        if getattr(exception, 'data', default=None):
+        if getattr(exception, 'data', None):
             result['error']['data'] = exception.data
 
         return result
