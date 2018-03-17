c5fc58eb3c726a01eaace8f7ca91e2a75f98f9f8
Antoine Lorence <antoine.lorence@gmail.com>
2018-03-18 00:30:48 +0100

    One more test module updated with new fixtures

--- modernrpc/tests/test_multicall_and_batch_requests.py
+++ modernrpc/tests/test_multicall_and_batch_requests.py
@@ -3,119 +3,108 @@ import json
 
 import pytest
 import requests
-from django.utils.six.moves import xmlrpc_client
+from django.utils.six.moves import xmlrpc_client as python_xmlrpc
 from jsonrpcclient.exceptions import ReceivedErrorResponse
-from jsonrpcclient.http_client import HTTPClient
 
 from modernrpc.exceptions import RPC_METHOD_NOT_FOUND, RPC_INTERNAL_ERROR, RPC_INVALID_REQUEST
-from test_authentication_system import get_url_with_auth
+from modernrpc.tests.test_authentication_system import get_url_with_auth
 
 
-def test_xmlrpc_multicall_standard(live_server):
+def test_xmlrpc_multicall_standard(xmlrpc_client):
 
-    client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
-
-    multicall = xmlrpc_client.MultiCall(client)
+    multicall = python_xmlrpc.MultiCall(xmlrpc_client)
     multicall.add(5, 10)
     multicall.divide(30, 5)
     multicall.add(8, 8)
     multicall.divide(6, 2)
     result = multicall()
 
-    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert isinstance(result, python_xmlrpc.MultiCallIterator)
     assert result[0] == 15  # 5 + 10
     assert result[1] == 6   # 30 / 5
     assert result[2] == 16  # 8 + 8
     assert result[3] == 3   # 6 / 2
 
 
-def test_xmlrpc_multicall_with_errors(live_server):
+def test_xmlrpc_multicall_with_errors(xmlrpc_client):
 
-    client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
-
-    multicall = xmlrpc_client.MultiCall(client)
+    multicall = python_xmlrpc.MultiCall(xmlrpc_client)
     multicall.add(7, 3)
     multicall.unknown_method()
     multicall.add(8, 8)
     result = multicall()
 
-    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert isinstance(result, python_xmlrpc.MultiCallIterator)
     assert result[0] == 10
-    with pytest.raises(xmlrpc_client.Fault) as excinfo:
+    with pytest.raises(python_xmlrpc.Fault) as excinfo:
         print(result[1])
     assert excinfo.value.faultCode == RPC_METHOD_NOT_FOUND
     assert result[2] == 16
 
 
-def test_xmlrpc_multicall_with_errors_2(live_server):
+def test_xmlrpc_multicall_with_errors_2(xmlrpc_client):
 
-    client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
-
-    multicall = xmlrpc_client.MultiCall(client)
+    multicall = python_xmlrpc.MultiCall(xmlrpc_client)
     multicall.add(7, 3)
     multicall.divide(75, 0)
     multicall.add(8, 8)
     result = multicall()
 
-    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert isinstance(result, python_xmlrpc.MultiCallIterator)
     assert result[0] == 10
-    with pytest.raises(xmlrpc_client.Fault) as excinfo:
+    with pytest.raises(python_xmlrpc.Fault) as excinfo:
         print(result[1])
     assert excinfo.value.faultCode == RPC_INTERNAL_ERROR
     assert result[2] == 16
 
 
-def test_xmlrpc_multicall_with_auth(live_server):
-    client = xmlrpc_client.ServerProxy(live_server.url + '/all-rpc/')
+def test_xmlrpc_multicall_with_auth(xmlrpc_client):
 
-    multicall = xmlrpc_client.MultiCall(client)
+    multicall = python_xmlrpc.MultiCall(xmlrpc_client)
     multicall.add(7, 3)
     multicall.logged_superuser_required(5)
     result = multicall()
 
-    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert isinstance(result, python_xmlrpc.MultiCallIterator)
     assert result[0] == 10
-    with pytest.raises(xmlrpc_client.Fault) as excinfo:
+    with pytest.raises(python_xmlrpc.Fault) as excinfo:
         print(result[1])
     assert excinfo.value.faultCode == RPC_INTERNAL_ERROR
     assert 'Authentication failed' in excinfo.value.faultString
 
 
-def test_xmlrpc_multicall_with_auth_2(live_server, superuser, common_pwd):
-    server_url = get_url_with_auth(live_server.url + '/all-rpc/', superuser.username, common_pwd)
-    client = xmlrpc_client.ServerProxy(server_url)
+def test_xmlrpc_multicall_with_auth_2(all_rpc_url, superuser, common_pwd):
 
-    multicall = xmlrpc_client.MultiCall(client)
+    server_url = get_url_with_auth(all_rpc_url, superuser.username, common_pwd)
+    client = python_xmlrpc.ServerProxy(server_url)
+
+    multicall = python_xmlrpc.MultiCall(client)
     multicall.add(7, 3)
     multicall.logged_superuser_required(5)
     result = multicall()
 
-    assert isinstance(result, xmlrpc_client.MultiCallIterator)
+    assert isinstance(result, python_xmlrpc.MultiCallIterator)
     assert result[0] == 10
     assert result[1] == 5
 
 
-def test_jsonrpc_multicall_error(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_multicall_error(jsonrpc_client):
 
     with pytest.raises(ReceivedErrorResponse) as excinfo:
-        c.request('system.multicall')
+        jsonrpc_client.request('system.multicall')
 
     assert 'Method not found' in excinfo.value.message
     assert excinfo.value.code == RPC_METHOD_NOT_FOUND
 
 
-def test_jsonrpc_batch_standard(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_standard(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': [5, 10]},
         {'jsonrpc': '2.0', 'id': 2, 'method': 'divide', 'params': [30, 5]},
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
 
@@ -123,9 +112,7 @@ def test_jsonrpc_batch_standard(live_server):
     assert result[1] == {'jsonrpc': '2.0', 'id': 2, 'result': 6}
 
 
-def test_jsonrpc_batch_with_errors(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_with_errors(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': [7, 3]},
@@ -133,7 +120,7 @@ def test_jsonrpc_batch_with_errors(live_server):
         {'jsonrpc': '2.0', 'id': 3, 'method': 'add', 'params': {'a': 2, 'b': 13}},
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert result[0] == {'jsonrpc': '2.0', 'id': 1, 'result': 10}
@@ -143,9 +130,7 @@ def test_jsonrpc_batch_with_errors(live_server):
     assert result[2] == {'jsonrpc': '2.0', 'id': 3, 'result': 15}
 
 
-def test_jsonrpc_batch_with_errors_2(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_with_errors_2(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': [7, 3]},
@@ -153,7 +138,7 @@ def test_jsonrpc_batch_with_errors_2(live_server):
         {'jsonrpc': '2.0', 'id': 3, 'method': 'add', 'params': (8, 8)},
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert result[0] == {'jsonrpc': '2.0', 'id': 1, 'result': 10}
@@ -165,9 +150,7 @@ def test_jsonrpc_batch_with_errors_2(live_server):
     assert result[2] == {'jsonrpc': '2.0', 'id': 3, 'result': 16}
 
 
-def test_jsonrpc_batch_with_named_params(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_with_named_params(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': {'a': 5, 'b': 10}},
@@ -177,7 +160,7 @@ def test_jsonrpc_batch_with_named_params(live_server):
         {'jsonrpc': '2.0', 'id': 5, 'method': 'method_with_kwargs_2', 'params': {'x': 25}},
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert result[0] == {'jsonrpc': '2.0', 'id': 1, 'result': 15}
@@ -188,9 +171,7 @@ def test_jsonrpc_batch_with_named_params(live_server):
     assert result[4] == {'jsonrpc': '2.0', 'id': 5, 'result': [25, '__json_rpc']}
 
 
-def test_jsonrpc_batch_with_notifications(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_with_notifications(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': {'a': 5, 'b': 10}},
@@ -198,7 +179,7 @@ def test_jsonrpc_batch_with_notifications(live_server):
         {'jsonrpc': '2.0', 'id': 2, 'method': 'divide', 'params': {'numerator': 30, 'denominator': 5}}
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert len(result) == 2
@@ -206,9 +187,7 @@ def test_jsonrpc_batch_with_notifications(live_server):
     assert result[1] == {'jsonrpc': '2.0', 'id': 2, 'result': 6}
 
 
-def test_jsonrpc_batch_notifications_only(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_notifications_only(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'method': 'add', 'params': {'a': 5, 'b': 10}},
@@ -216,21 +195,19 @@ def test_jsonrpc_batch_notifications_only(live_server):
         {'jsonrpc': '2.0', 'method': 'divide', 'params': {'numerator': 30, 'denominator': 5}}
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert result is None
 
 
-def test_jsonrpc_batch_with_auth(live_server):
-
-    c = HTTPClient(live_server.url + '/all-rpc/')
+def test_jsonrpc_batch_with_auth(jsonrpc_client):
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': {'a': 7, 'b': 3}},
         {'jsonrpc': '2.0', 'id': 2, 'method': 'logged_superuser_required', 'params': [5, ]}
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert result[0] == {'jsonrpc': '2.0', 'id': 1, 'result': 10}
@@ -240,27 +217,26 @@ def test_jsonrpc_batch_with_auth(live_server):
     }}
 
 
-def test_jsonrpc_batch_with_auth_2(live_server, superuser, common_pwd):
+def test_jsonrpc_batch_with_auth_2(jsonrpc_client, superuser, common_pwd):
 
-    c = HTTPClient(live_server.url + '/all-rpc/')
-    c.session.auth = (superuser.username, common_pwd)
+    jsonrpc_client.session.auth = (superuser.username, common_pwd)
 
     batch_request = json.dumps([
         {'jsonrpc': '2.0', 'id': 1, 'method': 'add', 'params': {'a': 7, 'b': 3}},
         {'jsonrpc': '2.0', 'id': 2, 'method': 'logged_superuser_required', 'params': [5, ]}
     ])
 
-    result = c.send(batch_request)
+    result = jsonrpc_client.send(batch_request)
 
     assert isinstance(result, list)
     assert result[0] == {'jsonrpc': '2.0', 'id': 1, 'result': 10}
     assert result[1] == {'jsonrpc': '2.0', 'id': 2, 'result': 5}
 
 
-def test_jsonrpc_batch_invalid_request(live_server):
+def test_jsonrpc_batch_invalid_request(all_rpc_url):
 
     headers = {'content-type': 'application/json'}
-    result = requests.post(live_server.url + '/all-rpc/', data='[1, 2, 3]', headers=headers).json()
+    result = requests.post(all_rpc_url, data='[1, 2, 3]', headers=headers).json()
 
     assert isinstance(result, list)
     assert len(result) == 3
