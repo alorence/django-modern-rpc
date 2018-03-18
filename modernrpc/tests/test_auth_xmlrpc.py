f8b4de33e6c65a7e040526d9ba84a2a47667a4e7
Antoine Lorence <antoine.lorence@gmail.com>
2018-03-18 11:37:11 +0100

    Auth tests refactoring (3/n): xmlrpc auth test module cleaned

--- modernrpc/tests/test_auth_xmlrpc.py
+++ modernrpc/tests/test_auth_xmlrpc.py
@@ -1,15 +1,7 @@
 # coding: utf-8
 import pytest
-from jsonrpcclient.exceptions import ReceivedErrorResponse
-from jsonrpcclient.http_client import HTTPClient
 
-from modernrpc.exceptions import RPC_INTERNAL_ERROR
 from . import python_xmlrpc
-
-
-# TODO: this should be unused at the end...
-def get_url_with_auth(orig_url, username, password):
-    return orig_url.replace('://', '://{uname}:{pwd}@').format(uname=username, pwd=password)
 
 
 class XmlRpcBase:
@@ -165,54 +157,45 @@ class TestAuthSuperuser(XmlRpcBase):
         assert xmlrpc_client_as_superuser.in_group_A_or_B_required(5) == 5
 
 
-
-def test_xmlrpc_user_permissions(live_server, john_doe, common_pwd, delete_user_perm, add_user_perm, change_user_perm):
-
-    johndoe_auth_url = get_url_with_auth(live_server.url + '/all-rpc/', john_doe.username, common_pwd)
-    c = python_xmlrpc.ServerProxy(johndoe_auth_url)
+def test_xmlrpc_user_permissions(xmlrpc_client_as_user, john_doe, delete_user_perm, add_user_perm, change_user_perm):
 
     john_doe.user_permissions.add(delete_user_perm)
 
-    assert c.delete_user_perm_required(5) == 5
-    assert c.any_permission_required(5) == 5
+    assert xmlrpc_client_as_user.delete_user_perm_required(5) == 5
+    assert xmlrpc_client_as_user.any_permission_required(5) == 5
 
     with pytest.raises(python_xmlrpc.ProtocolError) as exc_info:
-        c.all_permissions_required(5)
+        xmlrpc_client_as_user.all_permissions_required(5)
 
     assert exc_info.value.errcode == 403
 
     john_doe.user_permissions.add(add_user_perm, change_user_perm)
-    assert c.all_permissions_required(5) == 5
+    assert xmlrpc_client_as_user.all_permissions_required(5) == 5
 
 
-def test_xmlrpc_user_groups(live_server, john_doe, common_pwd, group_A, group_B):
-
-    johndoe_auth_url = get_url_with_auth(live_server.url + '/all-rpc/', john_doe.username, common_pwd)
-    c = python_xmlrpc.ServerProxy(johndoe_auth_url)
+def test_xmlrpc_user_groups(xmlrpc_client_as_user, john_doe, group_A, group_B):
 
     john_doe.groups.add(group_A)
 
-    assert c.in_group_A_required(5) == 5
-    assert c.in_group_A_or_B_required(5) == 5
+    assert xmlrpc_client_as_user.in_group_A_required(5) == 5
+    assert xmlrpc_client_as_user.in_group_A_or_B_required(5) == 5
 
-    assert pytest.raises(python_xmlrpc.ProtocolError, c.in_groups_A_and_B_required, 5).value.errcode == 403
-    assert pytest.raises(python_xmlrpc.ProtocolError, c.in_groups_A_and_B_required_alt, 5).value.errcode == 403
+    assert pytest.raises(python_xmlrpc.ProtocolError, xmlrpc_client_as_user.in_groups_A_and_B_required, 5).value.errcode == 403
+    assert pytest.raises(python_xmlrpc.ProtocolError, xmlrpc_client_as_user.in_groups_A_and_B_required_alt, 5).value.errcode == 403
 
     john_doe.groups.add(group_B)
 
-    assert c.in_groups_A_and_B_required(5) == 5
-    assert c.in_groups_A_and_B_required_alt(5) == 5
+    assert xmlrpc_client_as_user.in_groups_A_and_B_required(5) == 5
+    assert xmlrpc_client_as_user.in_groups_A_and_B_required_alt(5) == 5
 
 
-def test_custom_predicate_allowed(live_server):
+def test_custom_predicate_allowed(xmlrpc_client):
 
-    c = python_xmlrpc.ServerProxy(live_server.url + '/all-rpc/')
-    assert 'xmlrpc' in c.get_user_agent()
+    assert 'xmlrpc' in xmlrpc_client.get_user_agent()
 
 
-def test_custom_predicate_rejected(live_server):
+def test_custom_predicate_rejected(xmlrpc_client):
 
-    c = python_xmlrpc.ServerProxy(live_server.url + '/all-rpc/')
     with pytest.raises(python_xmlrpc.ProtocolError) as exc_info:
-        c.power_2(5)
+        xmlrpc_client.power_2(5)
     assert exc_info.value.errcode == 403
