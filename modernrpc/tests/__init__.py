3fc1e8b5929115e80d80d7b8da74606318090287
Antoine Lorence <antoine.lorence@gmail.com>
2018-03-18 15:55:37 +0100

    Coverage: import xmlrpc.client works on PY2
    'future' package is installed by default in test environment

--- modernrpc/tests/__init__.py
+++ modernrpc/tests/__init__.py
@@ -1,7 +1,11 @@
 # coding: utf-8
-try:
-    import xmlrpc.client as xmlrpclib
-except ImportError:
-    import xmlrpclib
+
+# Currently, test environment installs 'jsonrpcclient', which pulls 'future' package from pypi
+# As a result, the following Python 3 only import works well on Python2 too.
+# That's fine, let's work with that, but if for any reason, 'future' is not anymore installed
+# in test environment, this import should be securized with a try except, falling back to 'import xmlrpclib'
+# on Python 2.
+import xmlrpc.client as xmlrpclib
+
 
 __all__ = ['xmlrpclib']
