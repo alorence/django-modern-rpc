a2e4bde28454cda32dd5a9c47d0fa8483c6cb3e1
Antoine Lorence <antoine.lorence@gmail.com>
2018-04-28 12:32:31 +0200

    More tests refactoring

--- modernrpc/tests/test_init.py
+++ modernrpc/tests/test_init.py
@@ -6,8 +6,11 @@ from modernrpc.apps import check_required_settings_defined
 
 
 def test_settings_check(settings):
+
+    # With default testsite.settings, check method shouldn't complain
     assert len(check_required_settings_defined(apps.get_app_config("modernrpc"))) == 0
 
+    # After deleting values in settings.MODERNRPC_METHODS_MODULES, the check should warn
     settings.MODERNRPC_METHODS_MODULES = []
 
     result = check_required_settings_defined(apps.get_app_config("modernrpc"))
@@ -15,35 +18,29 @@ def test_settings_check(settings):
     assert result[0].id == 'modernrpc.E001'
 
 
-def test_init_import_warning(settings, rpc_registry):
-    # rpc_registry ficture is needed here, to ensure registry is restored after this test
-    settings.MODERNRPC_METHODS_MODULES = [
-        'invalid_module'
-    ]
-    app = apps.get_app_config("modernrpc")
-
-    with pytest.warns(Warning):
-        app.ready()
-
-
 def test_warning_on_invalid_module_reference(settings, rpc_registry):
-    rpc_registry.reset()
-    # Re-init library, with only invalid module as "methods module"
+
+    # Update setting value, to only contain reference to an invalid module
     settings.MODERNRPC_METHODS_MODULES = ['invalid.module.reference']
+
     # This will ensure a warning is thrown, due to invalid module specified
     app = apps.get_app_config("modernrpc")
     with pytest.warns(Warning):
         app.ready()
+
     # Now, we should have removed all custom methods from the registry
     custom_rpc_methods = [m for m in rpc_registry.get_all_method_names() if not m.startswith('system.')]
     assert len(custom_rpc_methods) == 0
 
 
 def test_registry_empty_if_settings_not_defined(settings, rpc_registry):
-    rpc_registry.reset()
-    # Re-init library, with None modules list
+
+    # Update setting value to remove any value
     settings.MODERNRPC_METHODS_MODULES = None
+
     app = apps.get_app_config("modernrpc")
     app.ready()
+
+    # Now, we should have removed all custom methods from the registry
     custom_rpc_methods = [m for m in rpc_registry.get_all_method_names() if not m.startswith('system.')]
     assert len(custom_rpc_methods) == 0
