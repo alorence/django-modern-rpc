d017af773a8812935e9b7370c54d5721dbb03c11
Antoine Lorence <antoine.lorence@gmail.com>
2018-04-28 12:27:00 +0200

    Docs

--- modernrpc/tests/conftest.py
+++ modernrpc/tests/conftest.py
@@ -10,6 +10,8 @@ from . import xmlrpclib, jsonrpclib
 def rpc_registry():
     # With Python 3.5+, we could use _dump = {**orig_dict} to easily perform a shallow copy
     # Unfortunately, we still maintain compatibility with Python 2.7, 3.3 & 3.4
+    # See https://www.python.org/dev/peps/pep-0448/
+    # See https://stackoverflow.com/a/46180556/1887976
     _registry_dump = modernrpc.core.registry._registry.copy()
     yield modernrpc.core.registry
     modernrpc.core.registry._registry = _registry_dump
