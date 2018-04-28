b89bde405d63b0563ec49b02800f9e12f65cb5ca
Antoine Lorence <antoine.lorence@gmail.com>
2018-04-28 12:18:05 +0200

    Tests refactoring

--- modernrpc/tests/test_coverage.py
+++ modernrpc/tests/test_coverage.py
@@ -3,10 +3,10 @@ import datetime
 
 from modernrpc.conf import settings
 from modernrpc.tests.test_registry import not_decorated_procedure
-from modernrpc.tests.test_rpc_method_object import dummy_function_with_doc
+from modernrpc.tests.test_rpc_method_class import dummy_function_with_doc
 from .test_registry import dummy_remote_procedure_1, dummy_remote_procedure_2, dummy_remote_procedure_3, \
     dummy_remote_procedure_4
-from .test_rpc_method_object import dummy_function, single_line_documented, multi_line_documented_1, \
+from .test_rpc_method_class import dummy_function, single_line_documented, multi_line_documented_1, \
     multi_line_documented_2
 from .testsite.rpc_methods_stub.generic import existing_but_not_decorated, another_name
 from .testsite.rpc_methods_stub.with_authentication import logged_user_required_alt, logged_superuser_required_alt, \
