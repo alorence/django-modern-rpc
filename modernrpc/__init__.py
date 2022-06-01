bd2c0efa6b0205ff0d24cf335f65f755f18566f2
Antoine Lorence <contact@alorence.me>
2022-06-01 20:35:56 +0200

    Stop defining default_app_config on Django 3.2+

--- modernrpc/__init__.py
+++ modernrpc/__init__.py
@@ -1,7 +1,11 @@
 # coding: utf-8
+from packaging.version import Version
+import django
 
-# default_app_config was deprecated in Django 3.2. Maybe set it only when detected django version is older?
-default_app_config = "modernrpc.apps.ModernRpcConfig"
+# Set default_app_config only with Django up to 3.1. This prevents a Warning on newer releases
+# See https://docs.djangoproject.com/fr/3.2/releases/3.2/#automatic-appconfig-discovery
+if Version(django.get_version()) < Version("3.2"):
+    default_app_config = "modernrpc.apps.ModernRpcConfig"
 
 # Package version is now stored in pyproject.toml only. To retrieve it from code, use:
 # import pkg_resources; version = pkg_resources.get_distribution('django-modern-rpc').version
