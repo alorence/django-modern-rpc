2fd5f0656434340763cc51c47238d4a40e61789b
Antoine Lorence <contact@alorence.me>
2022-06-13 10:47:22 +0200

    Remove unwanted dependency to 'packaging'

--- modernrpc/__init__.py
+++ modernrpc/__init__.py
@@ -1,10 +1,13 @@
 # coding: utf-8
-from packaging.version import Version
+from distutils.version import StrictVersion
+
 import django
 
-# Set default_app_config only with Django up to 3.1. This prevents a Warning on newer releases
-# See https://docs.djangoproject.com/fr/3.2/releases/3.2/#automatic-appconfig-discovery
-if Version(django.get_version()) < Version("3.2"):
+# distutils.version, overridden by setuptools._distutils.version in recent python releases, is deprecated
+# and will be removed in Python 3.12. We will probably drop Django < 3.2 until then, so this should be fine
+if StrictVersion(django.get_version()) < StrictVersion("3.2"):
+    # Set default_app_config only with Django up to 3.1. This prevents a Warning on newer releases
+    # See https://docs.djangoproject.com/fr/3.2/releases/3.2/#automatic-appconfig-discovery
     default_app_config = "modernrpc.apps.ModernRpcConfig"
 
 # Package version is now stored in pyproject.toml only. To retrieve it from code, use:
