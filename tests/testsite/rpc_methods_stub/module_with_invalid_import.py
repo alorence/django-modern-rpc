ade43c5fc9e925f032eb6f24c941cf7f855c302c
Antoine Lorence <contact@alorence.me>
2022-11-30 10:35:57 +0100

    Added pylama config and tox environment

--- tests/testsite/rpc_methods_stub/module_with_invalid_import.py
+++ tests/testsite/rpc_methods_stub/module_with_invalid_import.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # flake8: noqa
 from modernrpc.core import rpc_method
-from invalid_package import unknown_module
+from invalid_package import unknown_module  # noqa
 
 
 @rpc_method()
