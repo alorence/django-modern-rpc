8b672cac5e490902e46ca1d84485320df2c326bf
Antoine Lorence <alorence@flokod.com>
2024-02-22 13:40:08 +0100

    Solved error related to missing package in recent python versions

--- modernrpc/__init__.py
+++ modernrpc/__init__.py
@@ -1,10 +1,11 @@
 import django
-from packaging.version import Version
 
-if Version(django.get_version()) < Version("3.2"):
+if django.VERSION < (3, 2):
     # Set default_app_config only with Django up to 3.1. This prevents a Warning on newer releases
     # See https://docs.djangoproject.com/fr/3.2/releases/3.2/#automatic-appconfig-discovery
     default_app_config = "modernrpc.apps.ModernRpcConfig"
 
 # Package version is now stored only in pyproject.toml. To retrieve it from code, use:
-# import pkg_resources; version = pkg_resources.get_distribution('django-modern-rpc').version
+#   import pkg_resources; version = pkg_resources.get_distribution('django-modern-rpc').version
+# or in recent Python version (> 3.11), use
+#   import importlib.metadata; version = importlib.metadata.version("django-modern-rpc")
